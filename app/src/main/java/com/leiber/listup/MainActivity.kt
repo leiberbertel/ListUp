3864c42e6898f31d544aa3542bc13bf16f58a524
leiber bertel <leiberbertel777@gmail.com>
2025-10-16 21:59:22 -0500

    feat: add dummy playground files

--- app/src/main/java/com/leiber/listup/MainActivity.kt
+++ app/src/main/java/com/leiber/listup/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.leiber.listup.ui.components.HelloWorld
 import com.leiber.listup.ui.theme.ListUpTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ListUpTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    HelloWorld(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ListUpTheme {
-        Greeting("Android")
     }
 }
